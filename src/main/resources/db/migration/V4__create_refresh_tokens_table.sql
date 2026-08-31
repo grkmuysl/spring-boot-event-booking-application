b6f6a8b5baafb6deea7a3d1640f0d95f7fba1bc8
grkmuysl <gorkmuysal@gmail.com>
2026-08-31 12:43:43 +0300

    Fix the column name in the Refresh Tokens table

--- src/main/resources/db/migration/V4__create_refresh_tokens_table.sql
+++ src/main/resources/db/migration/V4__create_refresh_tokens_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE refresh_tokens(
 	 id BIGSERIAL PRIMARY KEY,
 	 token VARCHAR(512) NOT NULL UNIQUE,
 	 user_id BIGINT NOT NULL,
-	expiry_date TIMESTAMPTZ NOT NULL,
+	expiryDate TIMESTAMPTZ NOT NULL,
 	revoked BOOLEAN NOT NULL DEFAULT FALSE,
 	
 	CONSTRAINT fk_refresh_tokens_user FOREIGN KEY (user_id) REFERENCES users(id)
